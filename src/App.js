55f55e5eeddf7f4effb5387db90d28db40991ee1
Geethika <geethikakummetha@gmail.com>
2023-01-22 11:15:10 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import {
 
  
 function App() {
-  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [mode, setMode] = useState('light'); 
   const [alert, setAlert] = useState(null);
 
   const showAlert = (message, type)=>{
@@ -31,12 +31,6 @@ function App() {
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled", "success");
       document.title = 'TextUtils - Dark Mode';
-      // setInterval(() => {
-      //   document.title = 'TextUtils is Amazing Mode';
-      // }, 2000);
-      // setInterval(() => {
-      //   document.title = 'Install TextUtils Now';
-      // }, 1500);
     }
     else{
       setMode('light');
@@ -47,20 +41,15 @@ function App() {
   }
   return (
     <>
-    {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
-    {/* <Navbar/> */}
     <Router>
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
     <Alert alert={alert}/>
     <div className="container my-3">
     <Routes>
-    {/* /users --> Component 1
-        /users/home --> Component 2 */}
           <Route exact path="/about" element={<About />}>
-            
           </Route>
-          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>}>
-            
+          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils -Word Counter, Character Counter,
+          Remove Extra Spaces" mode={mode}/>}>
           </Route>
     </Routes>
     </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -52,15 +52,15 @@ export default function TextForm(props) {
   return (
     <>
     <div className='container' style={{color:props.mode==='dark'?'white':'#042743'}} >
-        <h1>{props.heading}</h1>
+        <h1 className='mb-4'>{props.heading}</h1>
         <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'grey':'white', color:props.mode==='dark'?'white':'black'}} id="exampleFormControlTextarea1" rows="8"></textarea>
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'#13466e':'white', color:props.mode==='dark'?'white':'black'}} id="exampleFormControlTextarea1" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
-        <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to LowerCase</button>
-        <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
-        <button className="btn btn-primary mx-2" onClick={handleVowelClick}>Vowels count</button>
-        <button className="btn btn-primary mx-2" onClick={handleConsonantClick}>Consonants count</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleLowClick}>Convert to LowerCase</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleClearClick}>Clear Text</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleVowelClick}>Vowels count</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleConsonantClick}>Consonants count</button>
 
 
 
@@ -68,10 +68,10 @@ export default function TextForm(props) {
     </div>
     <div className="container my-3"  style={{color:props.mode==='dark'?'white':'#042743'}}>
         <h1>Your text summary</h1>
-        <p>{text.split(" ").length-1} words and {text.length} characters</p>
-        <p>{0.008*text.split(" ").length}Minutes taken to read the text</p>
+        <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+        <p>{0.008*text.split(" ").filter((element)=>{return element.length!==0}).length}Minutes taken to read the text</p>
         <h2>Preview</h2>
-        <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
+        <p>{text.length>0?text:"Nothing to preview"}</p>
         <p>{count} No of vowels</p>
         <p>{count1} No of consonants</p>
     </div>
